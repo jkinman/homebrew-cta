3b2129bb8ca0d0db0916fa23d6e24370c46bbb9c
Joel Kinman <joel.kinman@highspot.com>
2025-07-10 01:00:31 -0700

    Update SHA256 checksum for v1.0 release

--- Formula/cta.rb
+++ Formula/cta.rb
@@ -2,7 +2,7 @@ class Cta < Formula
   desc "Cute and fluffy cat animation for when you mistype 'cat'"
   homepage "https://github.com/jkinman/cta"
   url "https://github.com/jkinman/cta/archive/refs/tags/v1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
   version "1.0"
 
