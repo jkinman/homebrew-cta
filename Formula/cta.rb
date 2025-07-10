16a59a7dd76abf7aea6645603d39de3f71da7c9a
Joel Kinman <joel.kinman@highspot.com>
2025-07-10 01:07:27 -0700

    Fix URL format for GitHub release

--- Formula/cta.rb
+++ Formula/cta.rb
@@ -1,7 +1,7 @@
 class Cta < Formula
   desc "Cute and fluffy cat animation for when you mistype 'cat'"
   homepage "https://github.com/jkinman/cta"
-  url "https://github.com/jkinman/cta/archive/refs/tags/v1.0.tar.gz"
+  url "https://github.com/jkinman/cta/archive/v1.0.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
   version "1.0"
